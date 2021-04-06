ded9ad487b8fe6dbb9bbc7ceebd8df9595e1f403
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2021-04-06 13:57:54 -0300

    agrega funcion para remover clase active

--- js/index.js
+++ js/index.js
@@ -4,8 +4,20 @@ d.addEventListener("keydown", (e) => {
   const $audio = d.querySelector(`audio[data-key="${e.code}"]`);
   const $key = d.querySelector(`section[data-key="${e.code}"]`);
 
+  if ($key) $key.classList.toggle("active");
+
   if (!$audio) return;
 
   $audio.currentTime = 0;
   $audio.play();
 });
+
+const $keys = d.querySelectorAll(".key");
+
+function removeActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("active");
+}
+
+$keys.forEach((key) => key.addEventListener("transitionend", removeActive));
